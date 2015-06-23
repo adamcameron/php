2795d9e9917288de869275153bc5631a61efa185
Adam Cameron <adam.cameron@hostelbookers.com>
2015-06-23 09:06:42 +0100

    PHP exception handling imrpoving file name

--- php.local/www/experiment/7/exceptionHandling/errorMishandling.php
+++ php.local/www/experiment/7/exceptionHandling/errorMishandling.php
@@ -1,8 +1,6 @@
 <?php
 // errorMishandling.php
 
-require __DIR__ . "/../../../../vendor/autoload.php";
-
 class Person {}
 
 class PersonFactory {
@@ -17,15 +15,21 @@ function doSomethingToPerson(Person $x){
 	return true;
 }
 
-$person = PersonFactory::getPersonFromId($_GET['id']);
-
+try {
+    $person = PersonFactory::getPersonFromId($_GET['id']);
+} catch(Exception $e) {
+	echo 'Exception caught<br>';
+    die;
+}
 try {
 	$result = doSomethingToPerson($person);
-	echo 'Process ran OK';
+	echo 'Process ran OK<br>';
 } catch(Exception $e) {
-	echo 'Exception caught';
-	new \dBug($e);
+	echo 'Exception caught<br>';
 } catch (EngineException $e){
-	echo 'EngineException caught';
-	new \dBug($e);
+	echo 'EngineException caught<br>';
+} finally {
+	echo 'In finally block<br>';
 }
+
+echo 'After try/catch/finally<br>';
