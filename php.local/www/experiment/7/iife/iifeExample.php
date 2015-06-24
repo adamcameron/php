9ba7fb7c8fae0ee04f2c8e9f1f08cfe8111d95a8
Adam Cameron <adam.cameron@hostelbookers.com>
2015-06-24 08:44:44 +0100

    PHP7: IIFE

--- php.local/www/experiment/7/iife/iifeExample.php
+++ php.local/www/experiment/7/iife/iifeExample.php
@@ -1,5 +1,5 @@
 <?php
-// usort.php
+// iifeExample.php
 
 class Score {
     private $value;
@@ -17,7 +17,6 @@ function getScores(){
     return $scores = array_map(function($x){return new Score($x);}, [97,2,47,73,29]);
 }
 
-/*
 $oldScoreSorter = function($e1, $e2){
     $e1Value = $e1->getValue();
     $e2Value = $e2->getValue();
@@ -26,34 +25,24 @@ $oldScoreSorter = function($e1, $e2){
     if ($e1Value > $e2Value) return 1;
     return -1;
 };
-*/
-
-$oldScoreSorter = function($e1, $e2){
-    return (function($x){
-        return (int) ($x / abs($x));
-    })($e1->getValue() - $e2->getValue());
-};
-
 
 
 $scores = getScores();
-echo '<h4>Before</h4><pre>';
-var_dump($scores);
-echo '</pre>';
-
 usort($scores, $oldScoreSorter);
 echo '<h4>After (using old approach)</h4><pre>';
 var_dump($scores);
 echo '</pre>';
 
-/*
-$newScoreSorter = function($e1, $e2){
-    return $e1->getValue() <=> $e2->getValue();
+
+$comparatorUsingIife = function($e1, $e2){
+    return (function($x){
+        return (int) ($x / abs($x));
+    })($e1->getValue() - $e2->getValue());
 };
+
 $scores = getScores();
-usort($scores, $newScoreSorter);
+usort($scores, $comparatorUsingIife);
 echo '<h4>After (using new approach)</h4><pre>';
 var_dump($scores);
 echo '</pre>';
-*/
 
