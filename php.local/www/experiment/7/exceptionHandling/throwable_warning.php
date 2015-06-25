70a99002ff87242d8a64242e7e2fa8afb896100f
Adam Cameron <adam.cameron@hostelbookers.com>
2015-06-25 09:04:43 +0100

    PHP7: throwables

--- php.local/www/experiment/7/exceptionHandling/throwable_warning.php
+++ php.local/www/experiment/7/exceptionHandling/throwable_warning.php
@@ -1,22 +1,12 @@
 <?php
 // throwable_warning.php
 
-echo 'Before everything<br>';
 try {
-    echo 'Before dodgy code<br>';
-    $result = 1 / 0; // generates a warning, so not caught :-|
-    echo 'After dodgy code<br>';
+    $result = 1 / 0;
 } catch (Throwable $t) {
     echo 'Throwable caught';
-    echo '<pre>';
-    var_dump($t);
-    echo '</pre>';
 } catch (Exception $e){
     echo 'Exception caught';
-    echo '<pre>';
-    var_dump($e);
-    echo '</pre>';
 } finally {
     echo 'In finally<br>';
 }
-echo 'After everything<br>';
