5461b09e4c8c44b2a0ef2ac57668bf01bc2f2169
Adam Cameron <adam.cameron@hostelbookers.com>
2015-06-25 09:01:28 +0100

    PHP7: throwables

--- php.local/www/experiment/7/exceptionHandling/throwable_error.php
+++ php.local/www/experiment/7/exceptionHandling/throwable_error.php
@@ -1,33 +1,16 @@
 <?php
-// throwable_warning.php
+// throwable_error.php
 
-echo 'Before everything<br>';
 try {
-    echo 'Before dodgy code<br>';
-
     function takesInt(int $x){
         return true;
     }
 
     takesInt('not an int');
-
-    echo 'After dodgy code<br>';
 } catch (Exception $e){
-    echo 'Exception caught';
-    echo '<pre>';
-    var_dump($e);
-    echo '</pre>';
-} catch (EngineException $e){
-    echo 'EngineException caught';
-    echo '<pre>';
-    var_dump($e);
-    echo '</pre>';
+    echo 'Exception caught<br>';
 } catch (Throwable $t) {
-    echo 'Throwable caught hi';
-    echo '<pre>';
-    var_dump($t);
-    echo '</pre>';
+    echo 'Throwable caught<br>';
 } finally {
     echo 'In finally<br>';
 }
-echo 'After everything<br>';
