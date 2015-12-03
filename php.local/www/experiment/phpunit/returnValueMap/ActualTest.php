2fd96ce223c6acac60ef58d2f4284eefbb1dd03c
Adam Cameron <adam.cameron@hostelbookers.com>
2015-12-03 14:19:51 +0000

    repro of PHPUnit weirdness with returnValueMap() (revising slightly)

--- php.local/www/experiment/phpunit/returnValueMap/ActualTest.php
+++ php.local/www/experiment/phpunit/returnValueMap/ActualTest.php
@@ -4,29 +4,47 @@ namespace returnValueMap;
 
 class ActualTest extends \PHPUnit_Framework_TestCase
 {
-    private $actual;
     private $mockedPackageResult = 'mocked_package_result';
     private $mockedPrepareResultPrefix = 'mocked_prepare_result';
     private $mockedProcessResultPrefix = 'mocked_process_result';
-
-    public function setup()
+/*
+    public function testActualiseWithMocksUsingReturnValueMap()
     {
-        $testObject = (object) ['property' => 'test property value'];
+        $actual = $this->getTestActualWithMocksUsingReturnValueMap();
+        $result = $actual->actualise(null);
 
-        $mockedPreparer = $this->mockPreparer();
-        $mockedProcessor = $this->mockProcessor($testObject);
-        $mockedPackager = $this->mockPackager();
-        $this->actual = new Actual($mockedPreparer, $mockedProcessor, $mockedPackager);
+        $this->assertEquals($this->mockedPackageResult, $result);
     }
-
-    public function testActualise()
+*/
+    public function testActualiseWithMocksUsingReturnCallback()
     {
-        $result = $this->actual->actualise(null);
+        $actual = $this->getTestActualWithMocksUsingReturnCallback();
+        $result = $actual->actualise(null);
 
         $this->assertEquals($this->mockedPackageResult, $result);
     }
 
-    private function mockPreparer()
+    private function getTestActualWithMocksUsingReturnValueMap()
+    {
+        $testObject = (object) ['property' => 'test property value'];
+
+        $mockedPreparer = $this->mockPreparerUsingReturnValueMap();
+        $mockedProcessor = $this->mockProcessorUsingReturnValueMap($testObject);
+        $mockedPackager = $this->mockPackager();
+        return new Actual($mockedPreparer, $mockedProcessor, $mockedPackager);
+    }
+
+    private function getTestActualWithMocksUsingReturnCallback()
+    {
+        $testObject = (object) ['property' => 'test property value'];
+
+        $mockedPreparer = $this->mockPreparerUsingWithCallback();
+        $mockedProcessor = $this->mockProcessorUsingWithCallback($testObject);
+        $mockedPackager = $this->mockPackager();
+        return new Actual($mockedPreparer, $mockedProcessor, $mockedPackager);
+    }
+
+    private function mockPreparerUsingReturnValueMap()
     {
         $mockedPrepareResultPrefix = $this->mockedPrepareResultPrefix;
         $valueMap = [
@@ -46,7 +64,23 @@ class ActualTest extends \PHPUnit_Framework_TestCase
         return $mockedPreparer;
     }
 
-    private function mockProcessor($testObject)
+    private function mockPreparerUsingWithCallback()
+    {
+        $mockedPreparer = $this->getMockBuilder('Preparer')
+            ->setMethods(['prepare'])
+            ->getMock();
+
+        $mockedPreparer
+            ->expects($this->exactly(2))
+            ->method('prepare')
+            ->will($this->returnCallback(function($value){
+                return "(MOCKED PREPARE: Value: $value)";
+            }));
+
+        return $mockedPreparer;
+    }
+
+    private function mockProcessorUsingReturnValueMap($testObject)
     {
         $mockedPrepareResultPrefix = $this->mockedPrepareResultPrefix;
         $mockedProcessResultPrefix = $this->mockedProcessResultPrefix;
@@ -67,10 +101,33 @@ class ActualTest extends \PHPUnit_Framework_TestCase
         return $mockedProcessor;
     }
 
+    private function mockProcessorUsingWithCallback($testObject)
+    {
+        $mockedProcessor = $this->getMockBuilder('Processor')
+            ->setMethods(['process'])
+            ->getMock();
+
+        $mockedProcessor
+            ->expects($this->at(0))
+            ->method('process')
+            ->with('(MOCKED PREPARE: Value: actual_preparation_flag_1)',null)
+            ->will($this->returnCallback([$this, 'mockedProcessCallback']));
+
+        $mockedProcessor
+            ->expects($this->at(1))
+            ->method('process')
+            ->with('(MOCKED PREPARE: Value: actual_preparation_flag_2)',null)
+            ->will($this->returnCallback([$this, 'mockedProcessCallback']));
+
+        return $mockedProcessor;
+    }
+
+    public function mockedProcessCallback($value, $object){
+        return "(MOCKED PROCESS: Value: $value)";
+    }
+
     private function mockPackager()
     {
-        $mockedProcessResultPrefix = $this->mockedProcessResultPrefix;
-        
         $mockedPackager = $this->getMockBuilder('Packager')
             ->setMethods(['package'])
             ->getMock();
@@ -81,8 +138,8 @@ class ActualTest extends \PHPUnit_Framework_TestCase
             ->with(
                 'actual_package_flag',
                 [
-                    'element1' => "{$mockedProcessResultPrefix}_1",
-                    'element2' => "{$mockedProcessResultPrefix}_2"
+                    'element1' => "(MOCKED PROCESS: Value: (MOCKED PREPARE: Value: actual_preparation_flag_1))",
+                    'element2' => "(MOCKED PROCESS: Value: (MOCKED PREPARE: Value: actual_preparation_flag_2))"
                 ]
             )
             ->willReturn($this->mockedPackageResult);
