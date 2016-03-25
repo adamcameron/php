440d4ff31471da50300a32b2199cefddf7831e46
Adam Cameron <cameron.adam@gmail.com>
2016-03-25 11:07:06 +0000

    mocking helper ground work

--- oo/codeExecution/src/WorkingSubClass.php
+++ oo/codeExecution/src/WorkingSubClass.php
@@ -7,12 +7,12 @@ echo __FILE__ . ' outside class definition' . PHP_EOL;
 
 class WorkingSubClass extends BaseClass {
 
-	public function __construct(){
+    public function __construct(){
         echo __FILE__ . ' constructor executed' . PHP_EOL;
 
-		require_once DEPENDENCY_DIR . '/Dependency.php';
+        require_once DEPENDENCY_DIR . '/Dependency.php';
         $dependency = new \other\Dependency();
         var_dump($dependency);
-	}
+    }
 
 }
