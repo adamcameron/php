1551cdb85ccf103dcf0597acc18121d5a4ee3638
Adam Cameron <dac.cfml@gmail.com>
2017-04-28 09:41:50 +0100

    sync

--- php.local/src/misc/surveyScratch.php
+++ php.local/src/misc/surveyScratch.php
@@ -3,23 +3,42 @@
 $conn = new PDO('mysql:host=localhost;dbname=scratch;port=3306', 'scratch', 'scratch');
 
 $answers = $conn->query('
-	SELECT q.id AS questionId, a.respondentId, a.answer
+	SELECT q.id AS questionId, q.question, a.respondentId, a.answer
 	FROM question q
 	INNER JOIN response a
 	ON q.id = a.questionId
-	WHERE q.id <= 3
-	ORDER BY q.id
-	LIMIT 20
+	where q.id <= 5
+	ORDER BY a.respondentId, q.id
 ');
 
-
-var_dump($answers);
-
-$groupedAnswers = [];
+$groupedByQuestion = [];
+$groupedByRespondent = [];
 foreach ($answers as $answer){
-	var_dump($answer);
-	$groupedAnswers[$answer['questionId']] = $groupedAnswers[$answer['questionId']] ?? [];
+	if (!isset($groupedByQuestion[$answer['questionId']])){
+		$groupedByQuestion[$answer['questionId']] = [
+			'id' => $answer['questionId'],
+			'question' => $answer['question'],
+			'answers' => []
+		];
+	}
+	$groupedByQuestion[$answer['questionId']]['answers'][$answer['respondentId']] = $answer['answer'];
 
+	if (!isset($groupedByRespondent[$answer['respondentId']])) {
+		$groupedByRespondent[$answer['respondentId']] = [];
+	}
+	$groupedByRespondent[$answer['respondentId']][$answer['questionId']] = $answer['answer'];
 }
 
-var_dump($groupedAnswers);
+echo '<ol>' . PHP_EOL;
+foreach ($groupedByQuestion as $question) {
+	echo "<li>" .  $question['question'] . "</li>" . PHP_EOL;
+}
+echo '</ol>' . PHP_EOL;
+foreach ($groupedByRespondent as $id=>$answer) {
+	echo "Respondent $id<br>";
+	echo "<blockquote><ol>";
+	foreach ($groupedByQuestion as $question) {
+		echo "<li>" .  $answer[$question['id']] . "</li>" . PHP_EOL;
+	}
+	echo "</ol></blockquote><hr>";
+}
