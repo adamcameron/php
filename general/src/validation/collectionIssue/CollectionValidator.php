b960e793dc626600e821d22905601c17112b9b6a
Adam Cameron <adam.cameron@hostelworld.com>
2018-10-13 11:07:36 +0100

    dunno what this is

--- general/src/validation/collectionIssue/CollectionValidator.php
+++ general/src/validation/collectionIssue/CollectionValidator.php
@@ -3,6 +3,7 @@
 namespace me\adamcameron\general\validation\collectionIssue;
 
 use Symfony\Component\Validator\Constraints\Collection;
+use Symfony\Component\Validator\Constraints\Optional;
 use Symfony\Component\Validator\Constraints\Type;
 use Symfony\Component\Validator\Validation;
 
@@ -21,18 +22,32 @@ class CollectionValidator
     private function setConstraints()
     {
         $this->constraints = [
+            new Type([
+                'type' => 'iterable',
+                'groups' => ['collection_check'],
+            ]),
             new Collection([
                 'fields' => [
-                    'field1' => [new Type(['type'=>'integer'])]
+                    'field1' => [
+                        new Type([
+                            'type'=>'integer',
+                            'groups' => ['fields_check'],
+                        ]),
+                    ],
                 ],
                 'allowMissingFields' => true,
-                'allowExtraFields' => true
+                'allowExtraFields' => true,
+                'groups' => ['fields_check'],
             ])
         ];
     }
 
     public function validate($value = null)
     {
-        return $this->validator->validate($value, $this->constraints);
+        $result = $this->validator->validate($value, $this->constraints, ['collection_check']);
+        if (count($result)) {
+            return $result;
+        }
+        return $this->validator->validate($value, $this->constraints, ['fields_check']);
     }
 }
